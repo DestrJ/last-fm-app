e874c5e7218f051d7dd4036eb80f0709082c961f
DestrJ <Destr17@gmail.com>
2020-02-10 23:49:14 +0200

    Updates

---
+++ precache-manifest.5f5f73dc7d2d613a7a9c29d585a66708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba81e8765ffba1ae703da56a9963d450",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a97193a8fea43f0aa3c7",
+    "url": "/static/css/main.6b0f4051.chunk.css"
+  },
+  {
+    "revision": "825973d6fbd508354048",
+    "url": "/static/js/2.36b23daa.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.36b23daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97193a8fea43f0aa3c7",
+    "url": "/static/js/main.e4d10802.chunk.js"
+  },
+  {
+    "revision": "820283524c935ec67003",
+    "url": "/static/js/runtime-main.dc991cf5.js"
+  }
+]);
